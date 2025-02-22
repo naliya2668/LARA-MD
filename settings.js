2dc13b1053325ebcbb6f16a189f0c9e2ea3fb5a5
naliya2668 <116560234+naliya2668@users.noreply.github.com>
2025-02-22 11:21:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "H24kyTob#R0znPeMRYIERMpg_cuJuZ2ecU4QRlA7XFRQpXeOJwjU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "nq4S3IAC#VDbh3oXh5Qd2IkEdbJQmzhWSnBTzRFI3ReTVV85iEos", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
